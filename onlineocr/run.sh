0c18b0605648560059cd852f58c105651459b052
林英杰 <linyingjie@ascs.tech>
2019-03-05 10:01:42 +0800

    init

---
+++ onlineocr/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pwd && ls -al
+
+#source /etc/profile && java -jar ./TestWeb.jar --server.port=9090
+source /etc/profile
+java -jar ./
---
+++ trigger.sh
@@ -0,0 +1,2 @@
+
+date>>tmp.txt; git add . --all; git commit -m '测试jenkins+git自动提交触发自动编译自动集成自动测试自动部署';git push;
